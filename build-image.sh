27e43eb5016bc683fae892c6491bf50caa2596ee
Artem Popov <artfwo@gmail.com>
2018-05-12 19:39:12 +0700

    temporarily build model with pi2-kernel

--- build-image.sh
+++ build-image.sh
@@ -2,7 +2,6 @@
 
 sudo ubuntu-image -c stable \
 	--extra-snaps gadget/cm3-norns-artfwo_16-1_armhf.snap \
-	--extra-snaps kernel/norns-kernel-artfwo_4.9-1_armhf.snap \
 	--extra-snaps norns/norns-test-artfwo_0.1_armhf.snap \
 	norns.model
 
--- flash-image.sh
+++ flash-image.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-sudo dd if=cm3.img of=/dev/sdb bs=32M status=progress
+sudo dd if=cm3.img of=/dev/sdb bs=4MiB status=progress
 sync
